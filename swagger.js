f0d5e75acd9a8191226ca2da63d5dfb6d473da0e
akarpovan <akarpovan@gmail.com>
2026-09-17 21:39:56 -0500

    added render host

--- swagger.js
+++ swagger.js
@@ -1,12 +1,22 @@
 const swaggerAutogen = require('swagger-autogen')();
 
-const doc = {
+/*const doc = {
     info: {
         title: 'Art API',
         description: 'API for managing artists and their artworks'
     },
     host: 'localhost:3000',
     schemes: ['http']
+};*/
+// Render define automáticamente la variable de entorno RENDER_EXTERNAL_HOSTNAME con su dominio real 
+// (cse341-w03-project2.onrender.com)
+const doc = {
+    info: {
+        title: 'Art API',
+        description: 'API for managing artists and their artworks'
+    },
+    host: process.env.RENDER_EXTERNAL_HOSTNAME || 'localhost:3000',
+    schemes: [process.env.RENDER_EXTERNAL_HOSTNAME ? 'https' : 'http']
 };
 
 const outputFile = './swagger.json';
